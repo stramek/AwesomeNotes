ed9263a84c564b0c6fb9abaf5da947176ea9e34f
Stramek <stramekz@gmail.com>
2015-11-16 01:51:13 +0100

    Android Studio push test

--- app/src/main/java/pl/xdcodes/stramek/awesomenotes/MainActivity.java
+++ app/src/main/java/pl/xdcodes/stramek/awesomenotes/MainActivity.java
@@ -16,6 +16,8 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    // Android studio push test
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
